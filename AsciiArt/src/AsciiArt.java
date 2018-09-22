e2f3f73e52630c184fb9a6852db9187c63325747
Josh <joshshlefstein@Joshs-MBP.home>
2018-09-22 16:36:01 -0400

    Star of David

--- AsciiArt/src/AsciiArt.java
+++ AsciiArt/src/AsciiArt.java
@@ -2,8 +2,26 @@
 public class AsciiArt {
 
 	public static void main(String[] args) {
-		System.out.println("\"Hello World\"");
-
+		System.out.println("Star of David");
+		System.out.println("		    /\\ \n"
+				+ "	           /  \\ \n"
+				+ "	          /    \\ \n"
+				+ "	         /      \\ \n"
+				+ "	________/________\\________ \n"
+				+ "	\\      /          \\      / \n"
+				+ "	 \\    /            \\    / \n"
+				+ "	  \\  /              \\  / \n"
+				+ "	   \\/                \\/ \n"
+				+ "	   /\\                /\\ \n"
+				+ "	  /  \\              /  \\ \n"
+				+ "	 /    \\            /    \\ \n"
+				+ "	/______\\__________/______\\ \n"
+				+ "	        \\        / \n"
+				+ "	         \\      / \n"
+				+ "	          \\    / \n"
+				+ "	           \\  / \n"
+				+ "	            \\/");
+		
 	}
 
 }
