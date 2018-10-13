d3085d4fdfb020ddb87706068199ef5b55b18731
Josh <joshshlefstein@Joshs-MBP.home>
2018-10-13 19:00:28 -0400

    FibonacciSequence

---
+++ ChapterFive/src/FibonacciSequenceChallenge.java
@@ -0,0 +1,20 @@
+
+public class FibonacciSequenceChallenge {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=0;
+		int c=0;
+		System.out.println(a);
+		for(int i=1; i<=7; i++) {
+			b=a+c;
+			System.out.println(b);
+			c=b+a;
+			System.out.println(c);
+			a=c+b;
+			System.out.println(a);
+			
+		}
+	}
+
+}
