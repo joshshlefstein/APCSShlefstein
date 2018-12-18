33caeba0c0c7f6fe649112e08f891e53ef4a07a9
Josh <33333466+joshshlefstein@users.noreply.github.com>
2018-12-18 15:24:28 -0500

    Vehicle

---
+++ Vehicle/src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car extends Vehicle {
+
+	public Car(String make, String model, int year, double price) {
+		super(make, model, year, price);
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Vehicle/src/Truck.java
@@ -0,0 +1,14 @@
+
+public class Truck extends Vehicle {
+
+	public Truck(String make, String model, int year, double price) {
+		super(make, model, year, price);
+
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
